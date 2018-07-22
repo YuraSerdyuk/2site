6716a8a8c5600e849a4b19e99ed3925ccf472fcb
Yura Serdyuk <yurko7203@gmail.com>
2018-07-22 20:58:05 +0300

    first12

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/', function(req, res) {
 
 
 http.listen(process.env.PORT || 5000, function(){
-    console.log('listening on: 5000 port');
+    console.log('Listening on: 5000 port');
 });
